b64a197fc261ffef96c11894937dc4e0b38d461a
centinela1024tb <centinela1024tb@gmail.com>
2023-09-01 18:43:22 -0600

    [ADD] se agrega conexión con @supabase a la tabla posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 
-export default function Home() {
+export default async function Home() {
+   const supabase = createServerComponentClient({ cookies })
+   const { data: posts} = await supabase.from('posts').select('*')
+
    return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
          Hola Twitter 
+         <pre>
+            {
+               JSON.stringify(posts, null, 2)
+            }            
+         </pre>
       </main>
    )
 }
